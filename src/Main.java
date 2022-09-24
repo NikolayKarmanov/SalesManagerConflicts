397c2b91b5d0a9b5905a1b2d3ba0d7bda855b022
Nikolay Karmanov <karmnic@gmail.com>
2022-09-24 10:15:19 +0300

    Change types INTEGER -> LONG

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {100, 50, 70, 90};
+        long[] sales = {100, 50, 70, 90};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println(salesManager.max());
     }
