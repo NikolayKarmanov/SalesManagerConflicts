1e51a21df2a4a89d50f59a7181a71779e1fdf48b
Nikolay Karmanov <karmnic@gmail.com>
2022-09-24 10:54:49 +0300

    Solve conflict

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -15,9 +15,9 @@ public class SalesManager {
         return max;
     }
 
-    public int min() {
-        int min = sales[0];
-        for (int sale : sales) {
+    public long min() {
+        long min = sales[0];
+        for (long sale : sales) {
             if (sale < min) {
                 min = sale;
             }
@@ -25,9 +25,9 @@ public class SalesManager {
         return min;
     }
 
-    public int croppedAverage() {
+    public long croppedAverage() {
         int sum = 0;
-        for (int sale : sales) {
+        for (long sale : sales) {
             sum += sale;
         }
         sum -= min() + max();
